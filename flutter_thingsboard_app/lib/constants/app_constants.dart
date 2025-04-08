6e2301ba366ea4d6ff8d5a5fb91ebb544e834433
Beckversync <bach.ngoce1108@hcmut.edu.vn>
2025-04-08 15:53:02 +0700

    Init

---
+++ flutter_thingsboard_app/lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+abstract class ThingsboardAppConstants {
+  static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+
+  static const thingsboardIOSAppSecret = 'Your app secret here';
+  static const thingsboardAndroidAppSecret = 'Your app secret here';
+}
